3487429fcccaf0718216047aa884b720bf8bde72
komalarao <manoharvakamullu1999@gmail.com>
2023-02-08 23:43:25 -0800

    refactor : regex program to test first name of user using junit

---
+++ userRegistrationTest/src/main/java/com/bridgelabz/userRegistrationTest/UserRegistration.java
@@ -0,0 +1,12 @@
+package com.bridgelabz.userRegistrationTest;
+
+import java.util.regex.*;
+
+public class UserRegistration {
+
+	public static final String VALIDATE_NAME = "^[A-Z]+[a-z]{2,}$";
+
+	public boolean checkFirstName(String firstName) {
+		return (Pattern.matches(VALIDATE_NAME, firstName));
+	}
+}
